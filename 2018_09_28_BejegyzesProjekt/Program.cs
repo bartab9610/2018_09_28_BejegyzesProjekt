f524267f3001049fd8f93a2c536fbc9595b0a015
bartab9610 <barnabas96@freemail.hu>
2018-09-28 14:47:28 +0200

    Get-Set függvények + konstruktor

--- 2018_09_28_BejegyzesProjekt/Program.cs
+++ 2018_09_28_BejegyzesProjekt/Program.cs
@@ -8,17 +8,69 @@ namespace _2018_09_28_BejegyzesProjekt
 {
     class Program
     {
-        string szerzo;
-        string tartalom;
-        int likeok;
-
-        static void Main(string[] args)
+        class Bejegyzes
         {
             // -szerzo : string
             // -tartalom : string
             // -likeok : int
             // -letrejott : DateTime
             // - szerkesztve : DateTime
+            string Szerzo;
+            string Tartalom;
+            public int Likeok = 0;
+            DateTime Letrejott = DateTime.Now;
+            DateTime Szerkesztve = DateTime.Now;
+
+            public int Like()
+            {
+                Likeok++;
+                return 0;
+            }
+            public Bejegyzes(string szerzo, string tartalom, int likeok, DateTime letrejott, DateTime szerkesztve)
+            {
+                this.Szerzo = szerzo;
+                this.Tartalom = tartalom;
+                this.Likeok = likeok;
+                this.Letrejott = letrejott;
+                this.Szerkesztve = szerkesztve;
+            }
+            public string szerzo
+            {
+                get
+                {
+                    return Szerzo; // string Szerzo;
+                }
+            }
+            public string tartalom
+            {
+                get
+                {
+                    return Tartalom; // string Tartalom;
+                }
+                set
+                {
+                    this.tartalom = Tartalom;
+                }
+            }
+            public int likeok
+            {
+                get
+                {
+                    return Likeok; // int Likeok = 0;
+                }
+            }
+            public DateTime letrejott
+            {
+                get
+                {
+                    return Letrejott = DateTime.Now;
+                }
+            }
+
+        }
+
+        static void Main(string[] args)
+        {
         }
     }
 }
