c61bbb2df54e07e7bf4e3a3f6a6b4820bf913376
bartab9610 <barnabas96@freemail.hu>
2018-09-28 14:14:36 +0200

    Osztály készítés

---
+++ 2018_09_28_BejegyzesProjekt/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2018_09_28_BejegyzesProjekt
+{
+    class Program
+    {
+        string szerzo;
+        string tartalom;
+        int likeok;
+
+        static void Main(string[] args)
+        {
+            // -szerzo : string
+            // -tartalom : string
+            // -likeok : int
+            // -letrejott : DateTime
+            // - szerkesztve : DateTime
+        }
+    }
+}
